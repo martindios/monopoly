cbfc34200da349cb8ddeabb59b18498bac6439fc
Pablomou6 <pabloml2005@gmail.com>
2024-10-13 12:11:21 +0200

    feat: (8) VER DESCRIPCION. Mods e correccións.
    Desc:
    -Casilla: Comprobar se o dueño é a banca en "pagarAlqquiler". Uso tmb de "sumarFortuna" e "sumarGastos".Tmb se añadiu que comprobara se é solvente de Impuestos. "sumarValor" feito, pero inda falta algo.
    -Dado: Línea para comprobar os dobles.
    -Jugador: getter de "EnCarcel"
    -Menu: Métodos para comprobar o solvente+pagar e tmb para comprobar se cae en IrCarcel. Override de "lanzarDados" para función específica da carcel.
    -Tablero: Corrección dalgún constructor que se usaba donde nn era.

--- src/partida/Dado.java
+++ src/partida/Dado.java
@@ -11,6 +11,7 @@ public class Dado {
     //Metodo para simular lanzamiento de un dado: devolverá un valor aleatorio entre 1 y 6.
     public int hacerTirada() {
         this.valor = this.random.nextInt(6) + 1;
+        //this.valor = 2; prueba encarcelar
         return valor;
     }
 
