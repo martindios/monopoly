d6fa93806a85d64a83ee4f86502df9041ded0bd2
Jorge González Corbelle <163537123+JorgeGonzalezCorbelle@users.noreply.github.com>
2024-11-18 13:51:13 +0100

    Interfaces

---
+++ src/monopoly/Consola.java
@@ -0,0 +1,10 @@
+package monopoly;
+
+import java.util.ArrayList;
+
+public interface Consola {
+    void imprimir(String mensaje);
+    void imprimirArray(ArrayList<String> mensaje);
+    String leer(String descripcion);
+
+}
---
+++ src/monopoly/Juego.java
@@ -0,0 +1,33 @@
+package monopoly;
+
+public class Juego {
+    public void comandoCrear(){
+        System.out.println("Todos los jugadores están registrados");
+    }
+
+    public void comando(){
+
+    }
+    public void comando(){
+
+    }
+    public void comando(){
+
+    }
+    public void comando(){
+
+    }
+    public void comando(){
+
+    }
+    public void comando(){
+
+    }
+    public void comando(){
+
+    }
+    public void comando(){
+
+    }
+
+}
